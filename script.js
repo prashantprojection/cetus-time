28a673f71da2e8f61a06144c671b3fd86cf1f980
Prashant Yadav <62587607+prashantprojection@users.noreply.github.com>
2021-12-08 14:46:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch('https://api.warframestat.us/pc/cetusCycle')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    document.getElementById("time").innerText = data.shortString;
+    }
+)
+.catch(function (err){
+    console.log(err);
+})
+
+let counter = 1;
+setInterval(() => {
+    counter++;
+    if(counter > 30) location.reload();
+}, 1000);
